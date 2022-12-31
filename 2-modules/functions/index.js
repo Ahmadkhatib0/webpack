f518b7e83e5df920e4d5594c362d05d917a5e464
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-12-31 23:56:12 +0300

    common js modules

---
+++ 2-modules/functions/index.js
@@ -0,0 +1,8 @@
+const { addAndMuliply, add } = require('./add')
+const subtract = require('./subtract')
+
+module.exports = {
+  addMultiply: addAndMuliply,
+  add,
+  subtract,
+}
---
+++ 2-modules/main.js
@@ -0,0 +1,5 @@
+const { add: addTowNumbers, subtract, addMultiply } = require('./functions')
+
+console.log(addTowNumbers(1, 3))
+console.log(addMultiply(3, 4, 5))
+console.log(subtract(3, 3))
