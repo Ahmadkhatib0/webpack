e03c6ae8e23ef8d126299883a6d08262827d7b4c
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-01-01 02:45:44 +0300

    add loaders project

---
+++ 3-loaders/src/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+import _ from 'lodash'
+
+document.getElementById('update-button').addEventListener('click', function () {
+  const el = document.getElementById('header')
+  el.innerHTML = 'this is updated version'
+
+  const listEl = document.getElementById('shopping-list')
+  const shoppingItems = ['Apple', 'orange', 'banana']
+
+  _.forEach(shoppingItems, function (item) {
+    const element = document.createElement('li')
+    element.innerHTML = item
+    listEl.appendChild(element)
+  })
+})
