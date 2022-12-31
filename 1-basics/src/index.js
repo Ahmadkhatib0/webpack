a69478e2e608bebf35cabc60f3b8f6fe24fe0c48
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2022-12-31 22:50:09 +0300

    init commit

---
+++ 1-basics/src/index.js
@@ -0,0 +1,12 @@
+function updateHeader() {
+  const el = document.getElementById('header')
+  const listEl = document.getElementById('shopping-list')
+  const shoppingItems = ['Apple', 'orange', 'banana']
+
+  _.forEach(shoppingItems, function (item) {
+    const element = document.createElement('li')
+    element.innerHTML = item
+    listEl.appendChild(element)
+  })
+  el.innerHTML = 'this is updated version'
+}
