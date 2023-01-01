bf19f225232d239e73af83f1d9983c2041314923
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-01-02 02:13:53 +0300

    add separate webpack files for each mode

--- 9-add-webpack-to-project/webpack.prod.js
+++ 9-add-webpack-to-project/webpack.prod.js
@@ -1,27 +1,13 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const MiniCss = require('mini-css-extract-plugin')
-const webpack = require('webpack')
 const { PurgeCSSPlugin } = require('purgecss-webpack-plugin')
 const glob = require('glob')
 
 const purgePath = { src: path.join(__dirname, 'src') }
 
 module.exports = {
-  entry: {
-    index: './src/index.js',
-    courses: './src/pages/courses.js',
-  },
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  devServer: {
-    static: './dist',
-  },
+  mode: 'production',
   module: {
     rules: [
       {
@@ -33,27 +19,9 @@ module.exports = {
         test: /\.s[ac]ss$/,
         use: [MiniCss.loader, 'css-loader', 'sass-loader'],
       },
-      {
-        test: /\.(png|jpeg|jpg|gif)$/,
-        type: 'asset/resource',
-      },
     ],
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      mnt: 'moment',
-      $: 'jquery',
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      chunks: ['index'],
-      filename: 'index.html',
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/pages/courses.html',
-      chunks: ['courses'],
-      filename: 'courses.html',
-    }),
     new CopyPlugin({
       patterns: [
         {
@@ -66,12 +34,6 @@ module.exports = {
     new PurgeCSSPlugin({
       paths: glob.sync(`${purgePath.src}/**/*`, { nodir: true }),
     }),
-    // new BundleAnalyzerPlugin({}),
     new MiniCss(),
   ],
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
 }
