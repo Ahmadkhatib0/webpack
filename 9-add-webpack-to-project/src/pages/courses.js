16ba9f3efe5a158aad798c3650d5adfa7dc42436
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-01-01 22:21:16 +0300

    add modal dialog

--- 9-add-webpack-to-project/src/pages/courses.js
+++ 9-add-webpack-to-project/src/pages/courses.js
@@ -1,19 +1,20 @@
-import courseData from '../data/courses.json'
-import bootstrap from 'bootstrap'
-import $ from 'jquery'
-import typed from 'typed.js'
-import validate from 'toastr'
-import toastr from 'toastr'
-import _ from 'lodash'
+import bootstrap from 'bootstrap';
+import $ from 'jquery';
+import typed from 'typed.js';
+import validate from 'validate.js';
+import toastr from 'toastr';
+import _ from 'lodash';
 
-import '../index.css'
-import '../_vendor.scss'
-import '../assets/css/font-awesome.min.css'
+import "../index.css";
+import "../_vendor.scss";
+import "../assets/css/font-awesome.min.css";
+
+import courseData from "../data/courses.json";
 
 function loadCourses() {
-  const courseListSection = $('#course-list')[0]
+  const courseListSection = $("#course-list")[0];
 
-  let courseItems = ''
+  let courseItems = "";
   _.each(courseData, (course) => {
     const courseItem =
       '<div class="card mb-3 rounded-3" style="max-width: 350px"><div class="row g-0 p-3"><div class="col-md-4"> <img src="' +
@@ -24,11 +25,11 @@ function loadCourses() {
       course.description +
       '</p><p class="card-text"><strong>Author: </strong><small class="text-muted">' +
       course.author +
-      '</small></p></div></div></div> </div>'
-    courseItems += courseItem
-  })
+      "</small></p></div></div></div> </div>";
+    courseItems += courseItem;
+  });
 
-  courseListSection.innerHTML = courseItems
+  courseListSection.innerHTML = courseItems;
 }
 
-loadCourses()
+loadCourses();
