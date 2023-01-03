e9d1895faae5bc9c81df38d388f8ee17ece3a109
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-01-03 22:00:48 +0300

    add @babel/polyfill in the entry point of SPA applicatoin to make webpack handles async stuff

--- 11-microfrontends/HomePageApp/src/index.js
+++ 11-microfrontends/HomePageApp/src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import '@babel/polyfill' // to make webpack work with async stuff
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'))
